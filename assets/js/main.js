4f4115e803bd9d290d9e0427a874e1bf3b5fb22b
Penumaka Vamsi Krishna Phaneendra <128373235+VamsiPenumaka@users.noreply.github.com>
2024-04-26 16:39:21 +0530

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,16 +227,16 @@
   new PureCounter();
 
 })()
-function reveal() {
-  var reveals = document.querySelectorAll(".krish");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+// function reveal() {
+//   var reveals = document.querySelectorAll(".krish");
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var elementTop = reveals[i].getBoundingClientRect().top;
+//     var elementVisible = 100;
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
